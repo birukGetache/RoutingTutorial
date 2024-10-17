a1f6de45e81970fed59e2e79795bfa491fc55b52
birukGetachew <burab1742@gmail.com>
2024-10-17 05:58:02 -0700

    books

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>404 not found these page</h4>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Jobs</h1>
+
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+        <RouterLink :to='{name:"JobDetails" , params:{id:job.id}}'>  <h2>{{ job.title }}</h2></RouterLink>
+      </div>
+  </div>
+  <div v-else>
+    <p>loading</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+data (){
+   
+    return{
+        jobs:[
+            
+        ]
+    }
+  
+},
+mounted(){
+        fetch('http://localhost:3000/jobs') 
+        .then(res => res.json() )
+        .then(data=>this.jobs = data)
+        .catch(err=>console.log(err))
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
